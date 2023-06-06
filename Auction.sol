d2245a9c11e9758c77e1eb62bc6bc31c50e86acf
MEHMET SORKULU <112237375+ZorkoMehmet@users.noreply.github.com>
2023-06-06 11:19:51 -0400

    placeBid start

--- Auction.sol
+++ Auction.sol
@@ -27,4 +27,44 @@ contract Auction{
         bidIncrement = 100;
         ipfsHash = "";
     }
+    
+    modifier onlyOwner(){
+        require(owner == msg.sender);
+        _;
+    }
+
+    modifier notOwner(){
+        require(owner != msg.sender);
+        _;
+    }
+
+    modifier afterStart(){
+        require(block.number >= startBlock);
+        _;
+    }
+
+    modifier beforeEnd(){
+        require(block.number <= endBlock);
+        _;
+    }
+
+    function changeOwner(address _owner) onlyOwner public{
+        owner == _owner;
+    }
+
+
+
+    function placeBid() notOwner afterStart beforeEnd public payable{
+        require(auctionState == State.Running);
+        require(msg.value >= 100);
+
+        uint currentBid = bids[msg.sender] + msg.value;
+        require(currentBid > highestBindingBid);
+
+        bids[msg.sender] = currentBid;
+
+        if(currentBid <= bids[highestBidder]){
+
+        }
+    }
 }
